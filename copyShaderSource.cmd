78499471fb17805d121c31e58455e42eeddef273
zuquanxie <designer_xzq@163.com>
2019-11-05 11:47:10 +0800

    1.调整复制着色器源文件的批处理

--- copyShaderSource.cmd
+++ copyShaderSource.cmd
@@ -1,7 +1,21 @@
 @echo off
-echo %1
-SET BUILDDIR="%1\out\build\x64-Debug\shaderSource"
-IF NOT EXIST %BUILDDIR% MKDIR %BUILDDIR%
-FOR /R "%1\shaderSource" %%i IN (*s) DO (
-	COPY %%i %BUILDDIR%
+:: 参数1为源文件夹，参数2为build后文件夹
+SET SDIR=%1
+SET BDIR=%2
+:: 转换路径连接符
+SET SDIR=%SDIR:/=\%
+SET BDIR=%BDIR:/=\%
+IF %SDIR%=="" (
+	GOTO end
+)
+:: 创建放置资源的文件夹
+IF NOT EXIST %BDIR% (
+	MKDIR %BDIR%
+	ECHO Create directory %BDIR%
+)
+:: 拷贝资源文件
+FOR /R %SDIR% %%i IN (*s) DO (
+	COPY %%i %BDIR% >NUL
+	ECHO Create source file %BDIR%\%%~NXi 
 ) 
+:end
